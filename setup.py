0e607c5b1e5d1ba208c481af878bbe22f37b55cf
Liji Alex <liji.alex@gmail.com>
2022-08-11 06:36:30 +0530

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 # Declaring variables for setup function
 PROJECT_NAME = "housing-predictor"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 AUTHOR = "Liji Alex"
 DESCRIPTION = "Housing prediction ml end to end project"
 PACKAGES = ["housing"]
@@ -18,7 +18,7 @@ def get_requirements_list()->List[str]:
         List[str]: A list containing the name of libraries in requirements.txt
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 
@@ -27,6 +27,7 @@ setup(
     version = VERSION,
     author = AUTHOR,
     description = DESCRIPTION,
-    packages = PACKAGES,
+    # packages = PACKAGES,
+    packages = find_packages(),
     install_requires = get_requirements_list()
 )
